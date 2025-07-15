107ffdfd03ba15f404db988aad311b8abae16f5b
SrdjanV <SrdjanV@protonmail.com>
2025-07-15 21:11:45 +0200

    Fix InMemorySqliteMonoDbContext

--- DAL/src/InMemorySqliteMonoDbContext.cs
+++ DAL/src/InMemorySqliteMonoDbContext.cs
@@ -4,8 +4,21 @@ namespace Mono.DAL;
 
 public class InMemorySqliteMonoDbContext : DefaultMonoDbContext
 {
+    public InMemorySqliteMonoDbContext()
+    {
+        Database.OpenConnection();
+        Database.EnsureCreated();
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=InMemorySample;Mode=Memory;Cache=Shared");
+    }
+
+    public override void Dispose()
+    {
+        //this class should be a singleton shared amongs all tests in a contexts
+        /*Database.CloseConnection();
+        base.Dispose();*/
     }
 }
